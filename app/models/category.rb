195186d98711d334d2af789151c4ac632291bd45
mre55 <misganawmihreteab>
2022-04-20 01:45:44 +0300

    Add `associations` and `validation` for each models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :expense_categories, dependent: :destroy
+  has_many :expenses, through: :expense_categories, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3 }
 end
