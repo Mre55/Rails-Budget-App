2e3a26ce1493be69c07bed75ef8b3c8d5ed70415
mre55 <misganawmihreteab>
2022-04-20 22:08:59 +0300

    Add style for `Category` and `Add category` pages

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,5 @@ class Category < ApplicationRecord
   has_many :expense_categories, dependent: :destroy
   has_many :expenses, through: :expense_categories, dependent: :destroy
 
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,6 +3,6 @@ class Expense < ApplicationRecord
   has_many :expense_categories, dependent: :destroy
   has_many :categories, through: :expense_categories, dependent: :destroy
 
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true
   validates :amount, numericality: { greater_than: 0, less_than: 1_000_000 }
 end
