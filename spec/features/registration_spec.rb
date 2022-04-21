5f44893dab73402baf08a41e02432501dd930a28
mre55 <misganawmihreteab>
2022-04-21 17:21:03 +0300

    Add test for `category_index` page

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature 'Signup', type: :feature do
  
   it 'user can see inputs and button' do
-    visit user_session_path
-    expect(page).to have_current_path(user_session_path)
+    visit new_user_registration_path
+    expect(page).to have_current_path(new_user_registration_path)
   end
 
 end
