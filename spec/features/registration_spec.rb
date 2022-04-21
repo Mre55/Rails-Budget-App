9efd347abbd7e6153ad24d3ba85477bf1432d6b9
mre55 <misganawmihreteab>
2022-04-21 17:16:36 +0300

    Add test for registration page

---
+++ spec/features/registration_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.feature 'Signup', type: :feature do
+ 
+  it 'user can see inputs and button' do
+    visit user_session_path
+    expect(page).to have_current_path(user_session_path)
+  end
+
+end
